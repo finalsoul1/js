16a6c5d820e2f18238a48816a6c0b513d7d06883
Kwon <finalsoul1@gmail.com>
2018-09-18 21:03:22 +0900

    0918C

--- book/chap21/427/427.js
+++ book/chap21/427/427.js
@@ -16,10 +16,9 @@ console.log(obj); // {a:10, b:20, c:30}
 
 console.log(Object.getOwnPropertyDescriptor(obj, 'a'));
 // { value: 10,
-//   writable: true, // 값 수정 가능
-//   enumerable: true, // console.log, for-in 처리가능
-//   configurable: true } // 프로퍼티 삭제 가능
+//   writable: true, // 값 수정 가능 (쓰기 가능한지)
+//   enumerable: true, // console.log, for-in 처리가능 (나열 가능한지)
+//   configurable: true } // 프로퍼티 삭제 가능 (설정 가능한지)
 
 console.log(Object.getOwnPropertyDescriptor(obj, 'b'));
 console.log(Object.getOwnPropertyDescriptor(obj, 'c'));
-
--- book/chap21/437/437.2.js
+++ book/chap21/437/437.2.js
@@ -4,7 +4,7 @@
   관객이 가수 객체를 사용하는 것은 똑같다.
   매니저(프락시)를 통해 가수 객체에 접근하게 되면
   매니저가 제공하는 부가 기능을 이용할 수 있는 혜택이 있다.
-  
+
   메타 프로그래밍이란?
   프로그램이 자기자신을 수정하는 것이다.
 */
@@ -18,7 +18,7 @@ const coefficients = {
   a: 1,
   c: 3,
 };
-// evaluate 함수 개발자의 의도와 다르게 부족한 값을 
+// evaluate 함수 개발자의 의도와 다르게 부족한 값을
 // 파라미터로 전달하면 계산이 엉망이 됩니다.
 // 어떻게 막을 수 있을까요?
 
@@ -33,4 +33,4 @@ const betterCoefficients = new Proxy(coefficients, {
 });
 
 let result = evaluate(5, betterCoefficients);
-console.log(result); // NaN
+console.log(result); // 76
